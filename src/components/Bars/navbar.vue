55c680cbed77fab43bfce239065be7e8b5feb400
Jorge E Gallardo <45702282+JorgeEGallardo@users.noreply.github.com>
2021-01-05 00:15:13 -0600

    first commit

---
+++ src/components/Bars/navbar.vue
@@ -0,0 +1,71 @@
+<template>
+<v-card>
+  <v-toolbar
+      color="white"
+      flat
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Page title</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs
+          v-model="model"
+          centered
+          slider-color="yellow"
+        >
+          <v-tab
+            :href="`#tab-${i}`"
+          >
+            INICIO
+          </v-tab>
+          <v-tab
+            :href="`#tab-${i}`"
+          >
+            CONOCENOS
+          </v-tab>
+          <v-tab
+            :href="`#tab-${i}`"
+          >
+            CATALOOGOS
+          </v-tab>
+          <v-tab
+            :href="`#tab-${i}`"
+          >
+            INSTALACIONES
+          </v-tab>
+          <v-tab
+            :href="`#tab-${i}`"
+          >
+            CONTACTO
+          </v-tab>
+<v-tab
+            :href="`#tab-${i}`"
+          >
+            BLOG
+          </v-tab>
+        </v-tabs>
+      </template>
+    </v-toolbar>
+</v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+ <Navbar></Navbar>
+    <v-btn color="success">Si ves un botón que no sea blanco funciono bien</v-btn>
+  </v-app>
+</template>
+
+<script>
+import Navbar from './Bars/navbar.vue';
+export default {
+  components: {Navbar},
+  data() {
+    return {
+    };
+  },
+};
+</script>
+
+<style>
+</style>>
